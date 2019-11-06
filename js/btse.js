f7e9e6bc2cec9f2dd8a7525070ed969602eb46da
romanornr <romanornr@gmail.com>
2019-11-06 16:37:05 +0100

    BTSE describe

---
+++ js/btse.js
@@ -0,0 +1,87 @@
+'use strict';
+
+//  ---------------------------------------------------------------------------
+
+const Exchange = require ('./base/Exchange');
+const { TICK_SIZE } = require ('./base/functions/number');
+const { AuthenticationError, ExchangeError, ArgumentsRequired, PermissionDenied, InvalidOrder, OrderNotFound, DDoSProtection, NotSupported, ExchangeNotAvailable, InsufficientFunds } = require ('./base/errors');
+
+module.exports = class btse extends Exchange {
+    describe() {
+        return this.deepExtend(super.describe(), {
+            'id': 'btse',
+            'name': 'BTSE',
+            'countries': ['UAE'],
+            'userAgent': undefined,
+            'rateLimit': 3000,
+            'has': {
+                'CORS': true,
+                'editOrder': true,
+                'fetchOrder': true,
+                'fetchOrders': false,
+                'fetchOpenOrders': true,
+                'fetchClosedOrders': true,
+                'fetchMyTrades': true,
+                'fetchTickers': false,
+            },
+            'urls': {
+                'test': 'https://testnet.btse.io',
+                'logo': '',
+                'api': {
+                    'web': 'https://www.btse.com',
+                    'api': 'https://api.btse.com',
+                    'testnet': 'https://testapi.btse.io'
+                },
+                'www': 'https://www.btse.com',
+                'doc': [
+                    'https://www.btse.com/apiexplorer/futures/',
+                    'https://www.btse.com/apiexplorer/spot/',
+                ],
+                'fees': 'https://support.btse.com/en/support/solutions/articles/43000064283-what-are-the-btse-trading-fees-',
+                'referral': 'https://www.btse.com/ref?c=0Ze7BK',
+            },
+            'api': {
+                'public': {
+                    'get': [
+                        'ping',
+                        'test',
+                        'getinstruments',
+                        'index',
+                        'getcurrencies',
+                        'getorderbook',
+                        'getlasttrades',
+                        'getsummary',
+                        'stats',
+                        'getannouncments',
+                    ],
+                },
+                'private': {
+                    'get': [
+                        'account',
+                        'getopenorders',
+                        'positions',
+                        'orderhistory',
+                        'orderstate',
+                        'tradehistory',
+                        'newannouncements',
+                    ],
+                    'post': [
+                        'buy',
+                        'sell',
+                        'edit',
+                        'cancel',
+                        'cancelall',
+                    ],
+                },
+            },
+            'exceptions': {
+                // 0 or absent Success, No error
+                '000': PermissionDenied, // "api_not_enabled" User didn't enable API for the Account
+            },
+            'precisionMode': TICK_SIZE,
+            'options': {
+                'fetchTickerQuotes': true,
+            },
+        });
+    }
+}
