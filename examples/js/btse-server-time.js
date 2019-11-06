c4b34c1eab6264a4d2193d9d607edf05efc92a82
romanornr <romanornr@gmail.com>
2019-11-06 18:02:10 +0100

    btse servertime example

---
+++ examples/js/btse-server-time.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const log  = require ('ololog').configure ({ locate: false })
+const ccxt = require('../../ccxt')
+
+const btse = new ccxt['btse'] ()
+const recvWindow = btse.options.recvWindow
+const aheadWindow = 1000
+
+async function test () {
+    const localStartTime = Date.now ()
+    const { serverTime } = await btse.spotv2getTime()
+    const localFinishTime = Date.now ()
+    const estimatedLandingTime = (localFinishTime + localStartTime) / 2
+
+    const diff = serverTime - estimatedLandingTime
+
+    log (`request departure time:     ${btse.iso8601 (localStartTime)}`)
+    log (`response arrival time:      ${btse.iso8601 (localFinishTime)}`)
+    log (`server time:                ${btse.iso8601 (serverTime)}`)
+    log (`request landing time (est): ${btse.iso8601 (estimatedLandingTime)}, ${Math.abs (diff)} ms ${Math.sign (diff) > 0 ? 'behind' : 'ahead of'} server`)
+    log ('\n')
+
+    if (diff < -aheadWindow) {
+        log.error.red (`your request will likely be rejected if local time is ahead of the server's time for more than ${aheadWindow} ms \n`)
+    }
+
+    if (diff > recvWindow) {
+        log.error.red (`your request will likely be rejected if local time is behind server time for more than ${recvWindow} ms\n`)
+    }
+}
+
+test ();
